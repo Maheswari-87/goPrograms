f3561aa29f179f99c44f60fdc37e6a6c91df36e3
Maheswarikoppoju <81303004+Maheswarikoppoju@users.noreply.github.com>
2021-04-06 10:36:28 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type I interface {
+	M()
+}
+
+func main() {
+	var i I
+	describe(i)
+	i.M()
+}
+
+func describe(i I) {
+	fmt.Printf("(%v, %T)\n", i, i)
+}
+
+//will get error as we have not assigned anything to inteface

0a78cd5fec8ca5656aeac63a0b01ad03c5456975
maheswarikoppoju <maheswari.koppoju@srsconsultinginc.com>
2021-04-15 12:24:39 +0530

    commit

--- dbconnection/dbconnection.go
+++ dbconnection/dbconnection.go
@@ -31,7 +31,7 @@ func main() {
 		panic(err.Error())
 	}
 
-	insert, err := db.Query("INSERT INTO godb.test (id,firstname,lastname) VALUES ('8','swetha','K');")
+	insert, err := db.Query("INSERT INTO godb.test (id,firstname,lastname) VALUES ('9','swathi','M');")
 	if err != nil {
 		panic(err.Error())
 	}
---
+++ rest-api2/go.mod
@@ -0,0 +1,5 @@
+module rest-api2.go
+
+go 1.16
+
+require github.com/gorilla/mux v1.8.0
